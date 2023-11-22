fc0ec56f252a6daaaf2efa792df770842b6d5676
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-11-22 13:58:38 -0800

    adding styling to the sidebar

--- app/Component/sidebar/sidebar.jsx
+++ app/Component/sidebar/sidebar.jsx
@@ -1,18 +1,28 @@
+"use server";
 import React from "react";
-import { FaUserFriends } from "react-icons/fa";
+import {
+  FaUserFriends,
+  FaHome,
+  FaSearch,
+  FaNotificationIcon,
+  FaCommunity,
+} from "react-icons/fa";
+import SidebarOption from "../sidebarOption/sidebarOption";
 
-const sidebar = () => {
+const Sidebar = () => {
   return (
     <>
       <div>
         <FaUserFriends />
-        <div>sidebar</div>
-        <div>sidebar</div>
-        <div>sidebar</div>
-        <div>sidebar</div>
+        <SidebarOption Icon={FaHome} text="Home" />
+        <SidebarOption Icon={FaHome} text="Profile" />
+        <SidebarOption Icon={FaHome} text="Notification" />
+        <SidebarOption Icon={FaHome} text="Community" />
+        <SidebarOption Icon={FaHome} text="Yard" />
+        <SidebarOption Icon={FaSearch} text="Explore" />
       </div>
     </>
   );
 };
 
-export default sidebar;
+export default Sidebar;
--- app/Component/sidebarOption/sidebarOption.jsx
+++ app/Component/sidebarOption/sidebarOption.jsx
@@ -1,7 +1,17 @@
+"use server";
 import React from "react";
+import "./sidebarOption.css";
 
-const sidebarOption = () => {
-  return <div>sidebarOption</div>;
+const sidebarOption = ({ text, Icon }) => {
+  const IconComponent = Icon && <Icon />;
+  return (
+    <>
+      <div className="sideBarOptionMain">
+        <div>{IconComponent}</div>
+        <div>{text}</div>
+      </div>
+    </>
+  );
 };
 
 export default sidebarOption;
