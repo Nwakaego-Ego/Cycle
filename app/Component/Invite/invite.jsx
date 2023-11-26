b9885fbcb5a141c5612f091a5c3b7a8faafd22f5
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-11-26 12:04:21 -0800

    styling invite

--- app/Component/Invite/invite.jsx
+++ app/Component/Invite/invite.jsx
@@ -50,55 +50,50 @@
 // }
 
 // invite.jsx
+
 import React from "react";
 import Image from "next/image";
 import "./invite.css";
+
 export default function Invite() {
+  const invitation = [
+    {
+      image: "/book.svg",
+      title: "Home School",
+      btn: "Join",
+    },
+    {
+      image: "/book.svg",
+      title: "Home School",
+      btn: "Join",
+    },
+    {
+      image: "/book.svg",
+      title: "Home School",
+      btn: "Join",
+    },
+  ];
+
   return (
     <div>
       <div className="invite_container">
         <h1>Invitation Request</h1>
-        <div className="flex invite-flex-container">
-          <Image
-            src="/book.svg"
-            alt="img profile"
-            width={50}
-            height={50}
-            layout="fit"
-            objectFit="cover"
-            className="invite_picture"
-          />
-          <h2 className="invite_profile">Sport Committee</h2>
-          <button className="invite_btn">Join</button>
-        </div>
-        <div className="flex invite-flex-container">
-          <div className="invite-picture-container">
-            <Image
-              src="/book.svg"
-              alt="img profile"
-              width={50}
-              height={50}
-              layout="fit"
-              objectFit="cover"
-              className="invite_picture"
-            />
-          </div>
-          <h2 className="invite_profile">Sport Committee</h2>
-          <button className="invite_btn">Join</button>
-        </div>
-        <div className="flex invite-flex-container">
-          <Image
-            src="/book.svg"
-            alt="img profile"
-            width={50}
-            height={50}
-            layout="fit"
-            objectFit="cover"
-            className="invite_picture"
-          />
-          <h2 className="invite_profile">Sport Committee</h2>
-          <button className="invite_btn">Join</button>
-        </div>
+
+        {invitation.map((invite) => {
+          return (
+            <div key={invite.id}>
+              <div className="flex invite-flex-container mb-4">
+                <img
+                  src={invite.image}
+                  alt={invite.title}
+                  className="invite_picture"
+                />
+                <h2 className="invite_profile ">{invite.title}</h2>
+                <button className="invite_btn">{invite.btn}</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
